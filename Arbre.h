5991a1e3eae0d9a5011e989b51e64be962cb43ab
Nizar ABAK-KALI <nizarabakkali93@gmail.com>
2014-12-05 20:12:25 +0100

    modif de ajout mot pour une parcours iteratif

--- Arbre.h
+++ Arbre.h
@@ -11,89 +11,72 @@
 
 typedef noeud* arbre;
 
-void ajouter_1noeudfilsgauche(noeud **a,char c){
-    noeud* noeudTmp ;
-    noeud* arbreTmp = *a ;
-    
-    noeud* elem = (arbre) malloc(sizeof(*elem));
-    elem->cle = c ;
+void ajouter_1noeudfilsgauche(noeud **a, char c) {
+    noeud* noeudTmp;
+    noeud* arbreTmp = *a;
+
+    noeud* elem = (arbre) malloc(sizeof (*elem));
+    elem->cle = c;
     elem->filsgauche = NULL;
-    elem->freredroit = NULL ;
-    
+    elem->freredroit = NULL;
+
     if (arbreTmp)
-        do{ 
-            noeudTmp = arbreTmp ;
-            if((int)c > (int)arbreTmp->cle){
-               
-                arbreTmp = arbreTmp->freredroit ;
-                if(!arbreTmp) noeudTmp->freredroit = elem ;
+        do {
+            noeudTmp = arbreTmp;
+            if ((int) c > (int) arbreTmp->cle) {
+
+                arbreTmp = arbreTmp->freredroit;
+                if (!arbreTmp) noeudTmp->freredroit = elem;
+            } else {
+
+                arbreTmp = arbreTmp->filsgauche;
+                if (!arbreTmp) noeudTmp->filsgauche = elem;
             }
-            else{
-            
-                arbreTmp = arbreTmp->filsgauche ;
-                if(!arbreTmp) noeudTmp->filsgauche = elem ;
-            }
-	}
-	while(arbreTmp);
+        } while (arbreTmp);
     else
-	*a = elem ;
+        *a = elem;
 }
 
-void ajoutermot(arbre *a,string mot,int cpt){
-    noeud* noeudTmp ;
-    noeud* arbreTmp = *a ;
-    
-    noeud* elem = (arbre) malloc(sizeof(*elem));
-    elem->cle = mot.at(0) ;
-    elem->filsgauche = NULL ;
-    elem->freredroit = NULL ;
-    
-    if(TEST)
-        cout<< "on ajoute : "<< 
-        ;
-    
-    
-    //on boucle sur le mot 
-    for(int c = 1 ; c < mot.length(); c++){
+void ajoutermot(arbre *a, string mot, int cpt) {
+    noeud* noeudTmp;
+    noeud* noeud_courant = *a;
+
+    if (TEST)
+        cout << "on ajoute : " << elem->cle << endl ;
+           
+
+    //on boucle sur chaque lettre du mot . 
+    for (int c = c; c < mot.length(); c++) {
+        noeud* n = (arbre) malloc(sizeof (*n));
+        n->cle = mot.at(c);
+        n->filsgauche = NULL;
+        n->freredroit = NULL;
         
-    if (arbreTmp)
-        do{ 
-            noeudTmp = arbreTmp ;
-            if((int)c > (int)arbreTmp->cle){
-               
-                arbreTmp = arbreTmp->freredroit ;
-                if(!arbreTmp) noeudTmp->freredroit = elem ;
-            }
-            else{
-            
-                arbreTmp = arbreTmp->filsgauche ;
-                if(!arbreTmp) noeudTmp->filsgauche = elem ;
-            }
-	}
-	while(arbreTmp);
-    else
-	*a = elem ;
-    
-    }  
+        //boucle ou je vais parcours les fils 
+        while(noeud_courant){
+          if(mot.at(c) == ){}  
+        }
+
+    }
 }
-void affiche_arbre_prefixe(arbre a , int profondeur){
-    if(!a) return ;
+
+void affiche_arbre_prefixe(arbre a, int profondeur) {
+    if (!a) return;
     else {
         a->affiche_noeud();
-        affiche_arbre_prefixe(a->filsgauche,profondeur);
-        affiche_arbre_prefixe(a->freredroit,profondeur++);
-    }    
+        affiche_arbre_prefixe(a->filsgauche, profondeur);
+        affiche_arbre_prefixe(a->freredroit, profondeur++);
+    }
 }
 
-void printTree(noeud *tree)
-{
-    if(!tree) return;
+void printTree(noeud *tree) {
+    if (!tree) return;
 
-    if(tree->filsgauche)  printTree(tree->filsgauche);
+    if (tree->filsgauche) printTree(tree->filsgauche);
 
     printf("Cle = %c\n", tree->cle);
 
-    if(tree->freredroit) printTree(tree->freredroit);
+    if (tree->freredroit) printTree(tree->freredroit);
 }
 
 
